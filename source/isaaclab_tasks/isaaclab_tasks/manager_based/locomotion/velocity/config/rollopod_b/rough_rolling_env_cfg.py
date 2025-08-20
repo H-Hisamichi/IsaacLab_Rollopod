e021c86f0b0c0ea79a3eda898e53aceec8654662
hisamichi <hisamichi@jaist.ac.jp>
2025-08-20 09:56:06 +0900

    Reward adjustments

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/rollopod_b/rough_rolling_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/rollopod_b/rough_rolling_env_cfg.py
@@ -33,17 +33,17 @@ class RollopodRewards(RewardsCfg):
     lin_vel_w_z_l2 = RewTerm(func=mdp.lin_vel_w_z_l2, weight=-0.5)
     #rolling_ang_vel = RewTerm(func=mdp.rolling_ang_vel, weight=0.1, params={"command_name": "base_velocity"})
     track_lin_vel_xy_w_exp = RewTerm(
-        func=mdp.track_lin_vel_xy_w_exp, weight=0.0, params={"command_name": "base_velocity", "std": math.sqrt(2.0)}
+        func=mdp.track_lin_vel_xy_w_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
     )
-    track_lin_vel_xy_w_exp_fine_grained = RewTerm(
-        func=mdp.track_lin_vel_xy_w_exp, weight=0.0, params={"command_name": "base_velocity", "std": math.sqrt(0.2)}
-    )
+    #track_lin_vel_xy_w_exp_fine_grained = RewTerm(
+    #    func=mdp.track_lin_vel_xy_w_exp, weight=0.0, params={"command_name": "base_velocity", "std": math.sqrt(0.2)}
+    #)
     track_rolling_ang_vel_exp = RewTerm(
-        func=mdp.track_com_ang_vel_z_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(2.0)}
+        func=mdp.track_com_ang_vel_z_exp, weight=2.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
     )
-    track_rolling_ang_vel_exp_fine_grained = RewTerm(
-        func=mdp.track_com_ang_vel_z_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.2)}
-    )
+    #track_rolling_ang_vel_exp_fine_grained = RewTerm(
+    #    func=mdp.track_com_ang_vel_z_exp, weight=2.0, params={"command_name": "base_velocity", "std": math.sqrt(0.2)}
+    #)
     # -- optional penalties
     flat_orientation_l2 = None
     flat_z_orientation_l2 = RewTerm(func=mdp.flat_z_orientation_l2, weight=0.0)
